6a9d7e0fb332a9beca49dfdbf91d0fb8c4c8047f
jeeva <jeeva26dinesh16@gmail.com>
2025-01-27 17:48:07 +0530

    Add project files.

---
+++ console calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace console_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter the operator(add,sub,mul,div):");
+            string cal = Console.ReadLine();
+            Console.WriteLine("first Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("second Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = 0;
+
+            switch(cal.ToLower())
+            {
+                case "add":
+                    c = a + b;
+                    break;
+                case "sub":
+                    c = a - b;
+                        break ;
+                    case "mul":
+                    c = a * b;
+                        break ;
+                    case "div":
+                    c = a / b ;
+                        break ;
+                default:
+                    Console.WriteLine("Invalid");
+                    return;
+
+
+            }
+            Console.WriteLine($"The Result is : {c}");
+            Console.ReadKey();
+        }
+    }
+}
